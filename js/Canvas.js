745665e35b8ca16db7c69a9d7a16215542c545a5
Thiago Ferreira <thiagodnf@gmail.com>
2022-12-01 18:42:55 -0500

    Initial commit

---
+++ js/Canvas.js
@@ -0,0 +1,87 @@
+export default class Canvas {
+
+    constructor(id) {
+        this.size = 15;
+        this.canvas = document.getElementById(id);
+        this.ctx = canvas.getContext("2d");
+        this.isMoveDown = false;
+        this.color = "black";
+    }
+
+    set width(width) {
+        this.canvas.width = width;
+    }
+
+    set height(height) {
+        this.canvas.height = height;
+    }
+
+    drawLine(x0, y0, x1, y1) {
+        this.ctx.beginPath();
+        this.ctx.lineWidth = 1;
+        this.ctx.moveTo(x0 + 0.5, y0 + 0.5);
+        this.ctx.lineTo(x1 + 0.5, y1 + 0.5);
+        this.ctx.stroke();
+    }
+
+    drawSquare(x, y, width = 10, height = 10, color = "red") {
+
+        this.ctx.beginPath();
+        this.ctx.fillStyle = color;
+        this.ctx.fillRect(x, y, width, height);
+        this.ctx.stroke();
+    }
+
+    drawGrid() {
+
+        let columns = parseInt(this.canvas.width / this.size) + 1;
+        let lines = parseInt(this.canvas.height / this.size) + 1;
+
+        let height = lines * this.size;
+        let width = columns * this.size;
+
+        for (let i = 1; i < lines + 1; i++) {
+            this.drawLine(0, i * this.size, width, i * this.size);
+        }
+
+        for (let i = 1; i < columns + 1; i++) {
+            this.drawLine(i * this.size, 0, i * this.size, height);
+        }
+    }
+
+    drawSquareAt(mousePosition) {
+
+        let i = parseInt(mousePosition.x / this.size);
+        let j = parseInt(mousePosition.y / this.size);
+
+        this.drawSquare(i * this.size, j * this.size, this.size, this.size, this.color);
+        this.drawGrid();
+    }
+
+    mouseDown(mousePosition) {
+
+        this.isMoveDown = true;
+
+        this.drawSquareAt(mousePosition);
+    }
+
+    mouseUp() {
+        this.isMoveDown = false;
+    }
+
+    mouseMove(mousePosition) {
+
+        if (this.isMoveDown) {
+            this.drawSquareAt(mousePosition);
+        }
+    }
+
+    clear(){
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.drawGrid();
+    }
+
+    update() {
+        this.clear();
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,70 @@
+import StorageUtils from "./utils/StorageUtils.js";
+import Canvas from "./Canvas.js";
+
+let c;
+let mousePosition = {
+    x: 0,
+    y: 0
+}
+
+function resizeWindow() {
+
+    $(".machine").height($(window).height() - 30*2)
+
+    $(".toolbar").height(100);
+
+    c.width = $(".toolbar").width()-6+2;
+    c.height = $(".machine").height() - $(".toolbar").height() - 20
+
+    c.update();
+}
+
+$(function () {
+
+    c = new Canvas("canvas");
+
+    $(window).resize(resizeWindow).trigger("resize");
+
+    $("canvas").mousedown(function() {
+        c.mouseDown(mousePosition);
+    }).mouseup(function() {
+        c.mouseUp(mousePosition);
+    }).mouseout(function() {
+        c.mouseUp(mousePosition);
+    }).mousemove(function(evt) {
+        var rect = c.canvas.getBoundingClientRect();
+
+        mousePosition.x = ((evt.clientX - rect.left) / (rect.right - rect.left) * c.canvas.width) - 2;
+        mousePosition.y = (evt.clientY - rect.top) / (rect.bottom - rect.top) * c.canvas.height - 2;
+
+        c.mouseMove(mousePosition);
+    });
+
+    $(".btn-color").change(function(){
+        c.color = this.value;
+    }).trigger("change")
+
+    $(".btn-clear").click(function(){
+        c.clear();
+    })
+
+    $(".btn-size").click(function(){
+
+        let size = parseInt(prompt("What's the size? [5,25]", c.size));
+
+        if (isNaN(size)) {
+            alert("This is not a number");
+            return;
+        }
+
+        if(size < 5 || size > 25){
+            alert("The size is not valid");
+            return;
+        }
+
+        c.size = size;
+        c.update();
+    })
+
+    c.update();
+});
